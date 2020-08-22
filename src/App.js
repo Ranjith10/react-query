b46bb63a1a430fba80b23dc7bfa9c8189a624614
Ranjith kumar <ranjit4072@gmail.com>
2020-08-22 15:10:50 +0530

    Added global query conig

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React from 'react'
 import { ReactQueryDevtools } from 'react-query-devtools'
+import { ReactQueryConfigProvider } from 'react-query'
 
 import ReactTable from './components/ReactTable'
+ 
+ const queryConfig = {
+   shared: {
+     suspense: false,
+   },
+   queries: {
+     enabled: true,
+     retry: 1,
+     retryDelay: attemptIndex => Math.min(1000 * 2 ** attemptIndex, 30000),
+     staleTime: 0,
+     cacheTime: 5 * 60 * 1000,
+     refetchOnWindowFocus: true,
+     refetchInterval: false,
+     refetchOnMount: true,
+     useErrorBoundary: false, // falls back to suspense
+   },
+   mutations: {
+     useErrorBoundary: false, // falls back to suspense
+   },
+ }
+ 
 
 function App() {
     return (
-        <div className = 'App'>
-            <ReactTable />
-            <ReactQueryDevtools />
-        </div>
+        <ReactQueryConfigProvider config = { queryConfig }>
+            <div className = 'App'>
+                <ReactTable />
+                <ReactQueryDevtools />
+            </div>
+        </ReactQueryConfigProvider>
     )
 }
 
